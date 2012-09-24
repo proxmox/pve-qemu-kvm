caba2cdc61104a362b244f93265c22f9b6df3f93
Dietmar Maurer <dietmar@proxmox.com>
2012-09-24 07:10:50 +0200

    use new internal-snapshot-async.patch patch

--- new/qemu-file.h
+++ new/qemu-file.h
@@ -68,7 +68,6 @@ QEMUFile *qemu_fopen_ops(void *opaque, QEMUFilePutBufferFunc *put_buffer,
 QEMUFile *qemu_fopen(const char *filename, const char *mode);
 QEMUFile *qemu_fdopen(int fd, const char *mode);
 QEMUFile *qemu_fopen_socket(int fd);
-QEMUFile *qemu_fopen_bdrv(BlockDriverState *bs, int is_writable);
 QEMUFile *qemu_popen(FILE *popen_file, const char *mode);
 QEMUFile *qemu_popen_cmd(const char *command, const char *mode);
 int qemu_stdio_fd(QEMUFile *f);
